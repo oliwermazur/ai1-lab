9daf2f101430e6ce972c94d708a4f24da25868ef
oliwermazur <oliwer.mazur4@gmail.com>
2024-01-03 11:33:18 +0100

    lab-f

--- lab-f/config/config.dist.php
+++ lab-f/config/config.dist.php
@@ -1,5 +1,5 @@
 <?php
 $config = [];
-$config['db_dsn'] = 'sqlite:I:/custom-php-framework/data.db';
+$config['db_dsn'] = 'sqlite:/path/to/data.db';
 $config['db_user'] = '';
 $config['db_pass'] = '';
